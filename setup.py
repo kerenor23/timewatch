aff6218fdb6623b0b0180dd2771f9a0cbcaccc93
kerenor <keren.or-curtis@corsight.ai>
2022-05-23 11:25:18 +0300

    make it work with new timewatch

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from distutils.core import setup
 import timewatch
 
 setup(
-  name = 'timewatch',
-  packages = ['timewatch'],
-  version = timewatch.__version__,
-  description = 'A library automating worktime reports for timewatch.co.il',
+  name='timewatch',
+  packages=['timewatch'],
+  version=timewatch.__version__,
+  description='A library automating worktime reports for timewatch.co.il',
   long_description=(open('README.md').read() if exists('README.md') else ''),
-  author = 'Nir Izraeli',
-  author_email = 'nirizr@gmail.com',
-  url = 'https://github.com/nirizr/timewatch',
-  download_url = 'https://github.com/nirizr/timewatch/tarball/{}'.format(timewatch.__version__),
-  keywords = ['timewatch', 'timewatch.co.il'], # arbitrary keywords
-  classifiers = [],
+  author='Keren-Or & Nir Izraeli',
+  author_email='nirizr@gmail.com',
+  url ='https://github.com/kerenor23/timewatch',
+  keywords=['timewatch', 'timewatch.co.il'], # arbitrary keywords
+  classifiers=[]
 )
--- timewatch/main.py
+++ timewatch/main.py
@@ -4,46 +4,41 @@ import argparse
 import datetime
 import logging
 
-import timewatch
+from timewatch import TimeWatch, Override
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Automatic work hours reporting for timewatch.co.il')
+    parser = argparse.ArgumentParser(description='Automatic work hours reporting for timewatch.co.il')
 
-  parser.add_argument('company', type=int, help='Company ID')
-  parser.add_argument('user', help='user name/id')
-  parser.add_argument('password', help='user password')
+    parser.add_argument('company', type=int, help='Company ID')
+    parser.add_argument('user', help='user name/id')
+    parser.add_argument('password', help='user password')
 
-  today = datetime.date.today()
-  parser.add_argument('-y', '--year', default=today.year, type=int, help='Year number to fill')
-  parser.add_argument('-m', '--month', default=today.month, help='Month number or name')
+    today = datetime.date.today()
+    parser.add_argument('-y', '--year', default=today.year, type=int, help='Year number to fill')
+    parser.add_argument('-m', '--month', default=today.month, help='Month number or name')
+    parser.add_argument('-v', '--verbose', default=0, action='count', help='increase logging level')
+    parser.add_argument('-o', '--override', default=Override.unreported.value, type=int,
+                        choices=[o.value for o in Override],
+                        help=f'Control override behavior: {Override.describe()}. '
+                             f'{Override.all.value} - override all working days, including ones already reported. unsafe to vacation/sick days. '
+                             f'{Override.work_days.value} - override all working days, including ones already reported, but keep vacation / sick days'
+                             f'{Override.unreported.value} - override only unreported days')
 
-  parser.add_argument('-v', '--verbose', default=0, action='count', help='increase logging level')
+    parser.add_argument('-s', '--start_time', default='10:00', help='punch-in time')
+    parser.add_argument('-j', '--jitter', default=0, type=int, help='punching time random range in minutes.')
+    parser.add_argument('-d', '--duration', default=9.0, type=float, help='hours duration (override preset edfault)')
 
-  parser.add_argument('-o', '--override', default='regular',
-                       choices=['all', 'incomplete', 'regular'],
-                       help='Control override behavior. all - override all '
-                            'working days, unsafe to vacation/sick days. '
-                            'incomplete = only override days with partial '
-                            'records. regular - override regular days '
-                            '(without absence reason) only')
+    args = parser.parse_args()
 
-  parser.add_argument('-s', '--starttime', default='10:00', help='punch-in time')
+    verbosity_levels = [logging.WARNING, logging.INFO, logging.DEBUG]
+    verbosity = min(args.verbose, len(verbosity_levels) - 1)
+    logging_level = verbosity_levels[verbosity]
 
-  parser.add_argument('-j', '--jitter', default=10, type=int,
-                       help='punching time random range in minutes.')
-
-  parser.add_argument('-r', '--retries', default=9, help='amount of times to retries on failed punchin')
-
-  args = parser.parse_args()
-
-  verbosity_levels = [logging.WARNING, logging.INFO, logging.DEBUG]
-  verbosity = min(args.verbose, len(verbosity_levels)-1)
-  logging_level = verbosity_levels[verbosity]
-
-  tw=timewatch.TimeWatch(loglevel = logging_level,
-                         override = args.override,
-                         starttime = args.starttime,
-                         jitter = args.jitter,
-                         retries = args.retries)
-  tw.login(args.company, args.user, args.password)
-  tw.edit_month(args.year, args.month)
+    tw = TimeWatch(loglevel=logging_level,
+                   override=args.override,
+                   start_time=args.start_time,
+                   jitter=args.jitter,
+                   duration=args.duration)
+    tw.login(args.company, args.user, args.password)
+    tw.edit_month(args.year, args.month)
